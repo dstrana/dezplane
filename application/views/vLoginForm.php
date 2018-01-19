3821c05c71aa0ff1af15f65cc056eefc9927fcca
dstrana <destrianaputrir@gmail.com>
2018-01-19 18:35:41 +0700

    Sprint 1 19/01/2018

---
+++ application/views/vLoginForm.php
@@ -0,0 +1,15 @@
+<HTML>
+	<head>
+		<title>
+			Login Form
+		</title>
+
+	</head>
+<body>
+	<form action="" method="POST">
+		<input type="text" name="username" placeholder="username">
+		<input type="password" name="password" placeholder="password">
+		<input type="submit" name="submit" placeholder="Login">
+	</form>
+</body>
+</html>
